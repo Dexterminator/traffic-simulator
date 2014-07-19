11627c0ac9f7d9249520172ab4ce51df2ccf3728
Dexter Gramfors <dexter.gramfors@gmail.com>
2014-07-19 16:10:03 +0200

    Added more files, git is super tricky :(

---
+++ Assets/ZoneScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneScript : MonoBehaviour {
+	public bool safe;
+	private int carsInside;
+
+	// Use this for initialization
+	void Start () {
+		safe = true;
+		carsInside = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (carsInside);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		carsInside++;
+		safe = carsInside == 0 ? true : false;
+	}
+
+	void OnTriggerExit(Collider other) {
+		carsInside--;
+		safe = carsInside == 0 ? true : false;
+	}
+}
